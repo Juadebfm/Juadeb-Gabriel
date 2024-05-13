99084b312e85a6764bbcc478dbb3b5cf737f1b04
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2024-05-13 11:28:15 +0100

    fix

--- src/components/Loader.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Loader = () => {
-  console.log("Loader component is rendered"); // Log when the Loader component is rendered
-  return <div>Loading...</div>; // Simplified loader content
-};
-
-export default Loader;
